bcd3c2d18dda3331ed79e9794b926ebe56faba9b
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-26 09:51:16 -0700

    Create digits.cpp

---
+++ digits.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,count=0;
+cout<<"Enter the integer : "<<endl;
+cin>>n;
+while(n!=0)
+{
+n/=10;
+++count;
+}
+cout<<"Number of digits in the given integer is : "<<count<<endl;
+return 0;
+}
