2eced80cc6f79f9b0e729f849734bf62d59ab6d1
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-26 11:28:05 -0700

    Create join.cpp

---
+++ join.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int N,K,a[N],temp;
+cout<<"Enter the N and : "<<endl;
+cin>>N>>K;
+cout<<"N and K joined value is : "<<N<<K<<endl;
+return 0;
+}
