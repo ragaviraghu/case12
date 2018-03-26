56512bbd40020e7429f2243d24232b5952861da2
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-26 11:07:01 -0700

    Create K_odd.cpp

---
+++ K_odd.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int N,K,a[N],count=0;
+cout<<"Enter the N and : "<<endl;
+cin>>N>>K;
+cout<<"Enter the N numbers : "<<endl;
+for(int i=1;i<=N;i++)
+{
+cin>>a[i];
+}
+for(int i=1;i<=N;i++)
+{
+if((a[i]%2)!=0)
+{
+count++;
+if(count==K)
+{
+cout<<"The Kth odd number is : "<<a[i]<<endl;
+break;
+}
+}
+}
+if(count==0)
+cout<<"There is no odd numbers"<<endl;
+if(count!=0 && count<K)
+cout<<"There is no Kth odd number"<<endl;
+return 0;
+}
